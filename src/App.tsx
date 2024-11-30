48179eef012b199e08814ef73542976b4037b409
Ben Foxall <benfoxall@gmail.com>
2024-11-30 08:39:59 +0000

    Add recorder

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import RecordAudio from "./RecordAudio";
 
 function App() {
   return (
     <>
       <h1>Audiovis</h1>
+      <RecordAudio onCreated={(b) => console.log("recorded", b)} />
     </>
   );
 }
---
+++ src/RecordAudio.tsx
@@ -0,0 +1,38 @@
+import { FC, useState } from "react";
+
+const RecordAudio: FC<{ onCreated: (blob: Blob) => void }> = ({
+  onCreated,
+}) => {
+  const [isRecording, setIsRecording] = useState(false);
+
+  const startRecording = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+    const recorder = new MediaRecorder(stream);
+    const chunks: Blob[] = [];
+    recorder.ondataavailable = (event) => {
+      chunks.push(event.data);
+    };
+    recorder.onstop = () => {
+      const audioBlob = new Blob(chunks, { type: recorder.mimeType });
+      onCreated(audioBlob);
+    };
+
+    recorder.start();
+    setIsRecording(true);
+    setTimeout(() => {
+      recorder?.stop();
+      setIsRecording(false);
+    }, 5000); // Stop recording after 5 seconds
+  };
+
+  return (
+    <div>
+      <button onClick={startRecording} disabled={isRecording}>
+        {isRecording ? "Recording..." : "Start Recording"}
+      </button>
+    </div>
+  );
+};
+
+export default RecordAudio;
